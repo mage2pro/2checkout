cf049971c59c1e6509597c813198913baae426a0
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-05-18 10:09:04 +0300

    no message

--- Source/Prefill.php
+++ Source/Prefill.php
@@ -2,9 +2,9 @@
 namespace Dfe\TwoCheckout\Source;
 class Prefill extends \Df\Config\SourceT {
 	/**
-	 * 2016-03-09
-	 * https://stripe.com/docs/testing#cards
-	 * https://mage2.pro/t/900
+	 * 2016-05-17
+	 * https://www.2checkout.com/documentation/sandbox/test-data/
+	 * https://mage2.pro/t/1631
 	 * @override
 	 * @see \Df\Config\Source::map()
 	 * @used-by \Df\Config\Source::toOptionArray()
@@ -13,14 +13,8 @@ class Prefill extends \Df\Config\SourceT {
 	protected function map() {
 		return [
 			0 => 'No'
-			, '4242424242424242' => 'Success (Visa)'
-			, '5555555555554444' => 'Success (MasterCard)'
-			, '4000000000000341' => 'Attempt to charge will fail'
-			, '4000000000000002' => 'The charge will be declined'
-			, '4100000000000019' => 'The charge will be fraudulent'
-			, '4000000000000069' => 'The card will be treated as expired'
-			, '4000000000000119' => 'A payment gateway processing error will be imitated'
-			, '4000000000000127' => 'The charge will be declined because of an incorrect CVC'
+			, '4000000000000002' => 'Success'
+			, '4333433343334333' => 'Failure'
 		];
 	}
 }
