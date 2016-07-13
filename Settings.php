67b12dd85d877238ed06f9eb564a818d7e9a9455
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2016-07-14 00:47:19 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -5,11 +5,10 @@ use Magento\Framework\App\ScopeInterface as S;
 class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2016-05-18
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	public function accountNumber($s = null) {
-		return $this->test($s) ? $this->testAccountNumber($s) : $this->liveAccountNumber($s);
+	public function accountNumber() {
+		return $this->test() ? $this->testAccountNumber() : $this->liveAccountNumber();
 	}
 
 	/**
@@ -28,36 +27,32 @@ class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2016-05-23
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Pass Order Items to the Payment Gateway?»
-	 * @param null|string|int|S $s [optional]
 	 * @return bool
 	 */
-	public function passOrderItems($s = null) {return $this->b(__FUNCTION__, $s);}
+	public function passOrderItems() {return $this->b(__FUNCTION__);}
 
 	/**
 	 * 2016-03-09
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Prefill the Payment Form with Test Data?»
 	 * @see \Dfe\TwoCheckout\Source\Prefill::map()
-	 * @param null|string|int|S $s [optional]
 	 * @return string|false
 	 */
-	public function prefill($s = null) {return $this->bv(__FUNCTION__, $s);}
+	public function prefill() {return $this->bv(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	public function publishableKey($s = null) {
-		return $this->test($s) ? $this->testPublishableKey($s) : $this->livePublishableKey($s);
+	public function publishableKey() {
+		return $this->test() ? $this->testPublishableKey() : $this->livePublishableKey();
 	}
 
 	/**
 	 * 2016-05-22
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	public function secretWord($s = null) {
-		return $this->test($s) ? $this->testSecretWord($s) : $this->liveSecretWord($s);
+	public function secretWord() {
+		return $this->test() ? $this->testSecretWord() : $this->liveSecretWord();
 	}
 
 	/**
@@ -70,124 +65,109 @@ class Settings extends \Df\Payment\Settings {
 	/**
 	 * 2016-05-18
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Live Account Number»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function liveAccountNumber($s = null) {return $this->v(__FUNCTION__, $s);}
+	private function liveAccountNumber() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-05-20
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Live API Password»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function livePassword($s = null) {return $this->p(__FUNCTION__, $s);}
+	private function livePassword() {return $this->p(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Live Publishable Key»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function livePublishableKey($s = null) {return $this->v(__FUNCTION__, $s);}
+	private function livePublishableKey() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Live Secret Key»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function liveSecretKey($s = null) {return $this->p(__FUNCTION__, $s);}
+	private function liveSecretKey() {return $this->p(__FUNCTION__);}
 
 	/**
 	 * 2016-05-22
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Live Secret Word»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function liveSecretWord($s = null) {return $this->p(__FUNCTION__, $s);}
+	private function liveSecretWord() {return $this->p(__FUNCTION__);}
 
 	/**
 	 * 2016-05-20
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Live API Username»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function liveUsername($s = null) {return $this->v(__FUNCTION__, $s);}
+	private function liveUsername() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-05-20
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function password($s = null) {
-		return $this->test($s) ? $this->testPassword($s) : $this->livePassword($s);
+	private function password() {
+		return $this->test() ? $this->testPassword() : $this->livePassword();
 	}
 
 	/**
 	 * 2016-03-02
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function secretKey($s = null) {
-		return $this->test($s) ? $this->testSecretKey($s) : $this->liveSecretKey($s);
+	private function secretKey() {
+		return $this->test() ? $this->testSecretKey() : $this->liveSecretKey();
 	}
 
 	/**
 	 * 2016-05-18
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Test Account Number»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function testAccountNumber($s = null) {return $this->v(__FUNCTION__, $s);}
+	private function testAccountNumber() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-05-20
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Sandbox API Password»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function testPassword($s = null) {return $this->p(__FUNCTION__, $s);}
+	private function testPassword() {return $this->p(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Test Publishable Key»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function testPublishableKey($s = null) {return $this->v(__FUNCTION__, $s);}
+	private function testPublishableKey() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-03-02
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Test Secret Key»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function testSecretKey($s = null) {return $this->p(__FUNCTION__, $s);}
+	private function testSecretKey() {return $this->p(__FUNCTION__);}
 
 	/**
 	 * 2016-05-22
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Sandbox Secret Word»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function testSecretWord($s = null) {return $this->p(__FUNCTION__, $s);}
+	private function testSecretWord() {return $this->p(__FUNCTION__);}
 
 	/**
 	 * 2016-05-20
 	 * «Mage2.PRO» → «Payment» → «2Checkout» → «Sandbox API Username»
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function testUsername($s = null) {return $this->v(__FUNCTION__, $s);}
+	private function testUsername() {return $this->v(__FUNCTION__);}
 
 	/**
 	 * 2016-05-20
-	 * @param null|string|int|S $s [optional]
 	 * @return string
 	 */
-	private function username($s = null) {
-		return $this->test($s) ? $this->testUsername($s) : $this->liveUsername($s);
+	private function username() {
+		return $this->test() ? $this->testUsername() : $this->liveUsername();
 	}
 }
 
