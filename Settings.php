841a8a4f19b39df024a2082f54663f7ffd868ac8
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-01-10 02:34:16 +0300

    no message

--- Settings.php
+++ Settings.php
@@ -13,8 +13,8 @@ final class Settings extends \Df\StripeClone\Settings {
 	 * 2016-05-19
 	 * https://github.com/2Checkout/2checkout-php#credentials-and-options
 	 * @override
-	 * @see \Df\StripeClone\Settings::init()
-	 * @used-by \Df\StripeClone\Method::api()
+	 * @see \Df\Payment\Settings::init()
+	 * @used-by \Df\Payment\Method::action()
 	 * @return void
 	 */
 	public function init() {
