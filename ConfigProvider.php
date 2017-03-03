783ce49e9180de6df1d2e299dc5763c6e125c49c
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-03-03 16:30:06 +0300

    no message

--- ConfigProvider.php
+++ ConfigProvider.php
@@ -1,5 +1,7 @@
 <?php
 namespace Dfe\TwoCheckout;
+// 2016-08-04
+// @used-by https://github.com/mage2pro/2checkout/blob/1.1.25/etc/frontend/di.xml?ts=4#L9
 /** @method Settings s() */
 final class ConfigProvider extends \Df\StripeClone\ConfigProvider {
 	/**
