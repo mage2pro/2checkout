7a8bf1244d48e53156df16e770480322dd559509
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-07-31 14:35:13 +0300

    1.4.14

--- Block/Info.php
+++ Block/Info.php
@@ -4,25 +4,6 @@ namespace Dfe\TwoCheckout\Block;
 /** @final Unable to use the PHP «final» keyword here because of the M2 code generation. */
 class Info extends \Df\Payment\Block\Info {
 	/**
-	 * 2016-05-23
-	 * @return string
-	 */
-	final function cardNumber() {return dfc($this, function() {return implode(
-		'********', $this->iia(self::CARD_F6, self::CARD_L2)
-	);});}
-
-	/**
-	 * 2016-05-23
-	 * @override
-	 * @see \Magento\Framework\View\Element\Template::getTemplate()
-	 * @see \Magento\Payment\Block\Info::$_template
-	 * @return string
-	 */
-	final function getTemplate() {return
-		'frontend' === $this->getArea() ? 'Dfe_TwoCheckout::info.phtml' : parent::getTemplate()
-	;}
-
-	/**
 	 * 2016-05-21
 	 * @override
 	 * @see \Df\Payment\Block\Info::prepare()
@@ -33,38 +14,29 @@ class Info extends \Df\Payment\Block\Info {
 			"https://{$this->isTest('sandbox.2checkout.com/sandbox', 'www.2checkout.com/va')}/"
 			,"sales/detail?sale_id={$this->iia(self::SALE_ID)}"
 		));
-		$this->si('Card Number', $this->cardNumber());
+		$this->si('Card Number', implode('········', $this->iia(self::CARD_F6, self::CARD_L2)));
 	}
 
 	/**
 	 * 2016-07-13
 	 * @override
 	 * @see \Df\Payment\Block\Info::testModeLabel()
-	 * @used-by \Df\Payment\Block\Info::markTestMode()
+	 * @used-by \Df\Payment\Block\Info::_prepareSpecificInformation()
 	 * @return string
 	 */
 	final protected function testModeLabel() {return 'Sandbox';}
 
 	/**
-	 * 2016-07-29
-	 * @override
-	 * @see \Df\Payment\Block\Info::testModeLabelLong()
-	 * @used-by \Df\Payment\Block\Info::title()
-	 * @return string
-	 */
-	final protected function testModeLabelLong() {return 'Sandbox Mode';}
-
-	/**
 	 * 2016-05-21
+	 * @used-by prepare()
 	 * @used-by \Dfe\TwoCheckout\Method::charge()
-	 * @used-by \Dfe\TwoCheckout\Block\Info::_prepareSpecificInformation()
 	 */
 	const CARD_F6 = 'first_six_digits';
 
 	/**
 	 * 2016-05-21
+	 * @used-by prepare()
 	 * @used-by \Dfe\TwoCheckout\Method::charge()
-	 * @used-by \Dfe\TwoCheckout\Block\Info::_prepareSpecificInformation()
 	 */
 	const CARD_L2 = 'last_two_digits';
 
