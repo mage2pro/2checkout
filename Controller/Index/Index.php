311a4effff6f555251b1adda28e10d2207d666bf
Dmitry Fedyuk <admin@dmitry-fedyuk.com>
2017-08-09 16:46:36 +0300

    no message

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -17,7 +17,10 @@ class Index extends \Magento\Framework\App\Action\Action {
 	/**
 	 * 2016-03-18
 	 * @override
-	 * @see \Magento\Framework\App\Action\Action::execute()
+	 * @see \Magento\Framework\App\Action\Action::execute()   
+	 * @used-by \Magento\Framework\App\Action\Action::dispatch():
+	 * 		$result = $this->execute();
+	 * https://github.com/magento/magento2/blob/2.2.0-RC1.8/lib/internal/Magento/Framework/App/Action/Action.php#L84-L125
 	 * @return Json
 	 */
 	function execute() {return df_leh(function() {return Json::i(Handler::p($this->params()));});}
